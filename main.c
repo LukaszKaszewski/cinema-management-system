01d0fdca44b7801ffa059b77bfdde82f19f9184c
Luk <90283345+LuKasz49864@users.noreply.github.com>
2022-12-16 22:30:40 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h> // getch();
+#include <string.h> // strcmp
+#include <ctype.h> // header
+#include "header1.h"
+
+
+ /*!
+ A small cinema management program
+ \date A.D. 2021
+ \author Lukasz Kaszewski
+ \link lukasz.kaszewski@10g.pl
+ */
+int main()
+{
+    login();
+    menu_glowne();
+}
